fc617df07733a14d4f13d3d23c9ba6aa9d7db356
Hakan Voski <123670567+HakanVoski@users.noreply.github.com>
2023-04-19 19:29:07 +0300

    Update project1.sql

--- project1.sql
+++ project1.sql
@@ -1,38 +1,35 @@
-## SQL Project 1
+-- ## SQL Project 1
 
-# film tablosunda bulunan title ve description sütunlarındaki verileri sıralayınız.
-
-SELECT title, description
-FROM Film
-WHERE length > 60 AND length < 75
-
----
-
-# Film tablosunda bulunan tüm sütunlardaki verileri film uzunluğu (length) 60 dan büyük VE 75 den küçük olma koşullarıyla sıralayınız.
+-- Film tablosunda bulunan title ve description sütunlarındaki verileri sıralayınız.
 
 SELECT title, description
 FROM Film
 WHERE length > 60 AND length < 75
 
----
 
-# Film tablosunda bulunan tüm sütunlardaki verileri rental_rate 0.99 VE replacement_cost 12.99 VEYA 28.99 olma koşullarıyla sıralayınız.
+
+-- Film tablosunda bulunan tüm sütunlardaki verileri film uzunluğu (length) 60 dan büyük VE 75 den küçük olma koşullarıyla sıralayınız.
+
+SELECT title, description
+FROM Film
+WHERE length > 60 AND length < 75
+
+
+-- Film tablosunda bulunan tüm sütunlardaki verileri rental_rate 0.99 VE replacement_cost 12.99 VEYA 28.99 olma koşullarıyla sıralayınız.
 
 SELECT title, description
 FROM Film
 WHERE rental_rate = 0.99 AND (replacement_cost = 12.99 OR replacement_cost = 28.99)
 
----
 
-# Customer tablosunda bulunan first_name sütunundaki değeri 'Mary' olan müşterinin last_name sütunundaki değeri nedir?
+-- Customer tablosunda bulunan first_name sütunundaki değeri 'Mary' olan müşterinin last_name sütunundaki değeri nedir?
 
 SELECT *
 FROM Customer
 WHERE first_name = 'Mary'
 
----
 
-# Film tablosundaki uzunluğu(length) 50 ten büyük OLMAYIP aynı zamanda rental_rate değeri 2.99 veya 4.99 OLMAYAN verileri sıralayınız.
+-- Film tablosundaki uzunluğu(length) 50 ten büyük OLMAYIP aynı zamanda rental_rate değeri 2.99 veya 4.99 OLMAYAN verileri sıralayınız.
 
 SELECT * 
 FROM Film
