75d78193c6545713a92e2ba28b0ff505e602a68f
Hakan Voski <123670567+HakanVoski@users.noreply.github.com>
2023-04-22 20:18:58 +0300

    Update project2.sql

--- project2.sql
+++ project2.sql
@@ -14,7 +14,7 @@ FROM actor
 WHERE first_name IN ('Penelope', 'Nick', 'Ed')
 
 
--- Film tablosunda bulunan tüm sütunlardaki verileri rental_rate 0.99, 2.99, 4.99 VE replacement_cost 12.99, 15.99, 28.99 olma koşullarıyla sıralayınız. ( IN operatörünü kullanınız.)*/
+-- Film tablosunda bulunan tüm sütunlardaki verileri rental_rate 0.99, 2.99, 4.99 VE replacement_cost 12.99, 15.99, 28.99 olma koşullarıyla sıralayınız.
 
 SELECT *
 FROM film
