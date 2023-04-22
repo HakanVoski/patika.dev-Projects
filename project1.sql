c16edcb54c4f202d3196b776f6c5c6db4a189c1a
Hakan Voski <123670567+HakanVoski@users.noreply.github.com>
2023-04-22 20:11:37 +0300

    Update project1.sql

--- project1.sql
+++ project1.sql
@@ -4,8 +4,6 @@
 
 SELECT title, description
 FROM Film
-WHERE length > 60 AND length < 75
-
 
 
 -- Film tablosunda bulunan tüm sütunlardaki verileri film uzunluğu (length) 60 dan büyük VE 75 den küçük olma koşullarıyla sıralayınız.
@@ -24,7 +22,7 @@ WHERE rental_rate = 0.99 AND (replacement_cost = 12.99 OR replacement_cost = 28.
 
 -- Customer tablosunda bulunan first_name sütunundaki değeri 'Mary' olan müşterinin last_name sütunundaki değeri nedir?
 
-SELECT *
+SELECT last_name
 FROM Customer
 WHERE first_name = 'Mary'
 
